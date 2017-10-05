2a8518d37a5743b694e0354e1b9662050a8b798f
finolex <hermitdudz@gmail.com>
2017-10-05 14:31:29 -0400

    thursday week 5 class and hw

--- p5/Week 5/Homework Tuesday/sketchfancy.js
+++ p5/Week 5/Homework Tuesday/sketchfancy.js
@@ -9,12 +9,14 @@ function setup(){
 
 function draw(){
 	background(255);
-	var mouseMap = map(mouseX, 0, width, 1, 5);
+	var mouseMap = map(mouseX, 0, width, 2, 10);
+	var mouseScale = map(mouseY, 0, height, 1 ,1.5)
 	for(var i = 0; i < 4; i++){
 		for(var j = 0; j < 4; j++){
 		push();
 		translate((i*200),(j*200));
 		rotate(-2*PI/(mouseMap));
+		scale(mouseScale);
 		rect(i,j,100,100);
 		pop();
 		}
---
+++ p5/Week 5/Thursday/sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+	createCanvas(600,600);
+}
+
+function draw(){
+	background(255);
+	var mouseXMap = map (mouseX, 0, width, 0, 2);
+	var mouseYMap = map (mouseY, 0, height, 0, 2);
+	for(var i=0; i < 10; i++){
+		for(var j=0; j < 10; j++){
+			push();
+			fill((i*20)*mouseXMap, ((j*20))*mouseYMap, ((i*j*10))*mouseXMap*mouseYMap);
+			rect(i*100, j*100, 100, 100);
+			for(var k = 0; k < 5; k++){
+				stroke(255-(i*10), 255-(j*20), 255-(k*30));
+				strokeWeight(2);
+				line(mouseXMap*(100/3)*((4*k)+1), j, (100/3)*((4*k)+1), mouseYMap*j*100);
+			}
+			pop();
+		}
+	}
+
+}
